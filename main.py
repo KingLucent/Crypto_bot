ba3547821562c7c1349225e502095ffa7f48de17
KingLucent <52358087+KingLucent@users.noreply.github.com>
2019-11-11 20:49:01 +0300

    New version
    Stable
    /start /help /lection /practice 
    Links for 2 lections and 1 task

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ def handle(msg):
     print(content_type, chat_type, chat_id)
     if content_type == 'text' and msg['text'].startswith('/'):
         if msg['text'] == '/start':
-            bot.sendMessage(chat_id, 'Welcome dear friend👩‍💻 👨‍💻! To begin press🔑 /help')
+            bot.sendMessage(chat_id, 'Welcome dear friend👩‍💻 👨‍💻!\nДля початку натисни🔑 /help \nЯкщо ти знайомий з ботом, то /menu')
         elif msg['text'] == '/help':
             bot.sendMessage(chat_id, 'Застряг🤯? Зараз допоможемо🤖. Ознайомся з цим гайдом: https://telegra.ph/Korotkij-ekskurs-po-botu-11-04')
         elif msg['text'] == '/menu':
             bot.sendMessage(chat_id, 'Почати вивчення криптографії😎- \n /lection \n Спробувати свої сили🕵️‍♀️🕵️‍♂️- \n /practice')
         elif msg['text'] == '/lection':
-            bot.sendMessage(chat_id,'/l1 - Ceasar \n /l2 \n /l3  \n /l4 \n /l5' )
+            bot.sendMessage(chat_id,'/l1 - Шифр Цезаря \n /l2- SHifr-Vizhenera\n /l3- Машина Беббіджа  \n /l4- \n /l5-' )
         elif msg['text'] == '/l1':
             bot.sendMessage(chat_id,'https://telegra.ph/SHifr-Cezarya-11-06' ) #1 lection
         elif msg['text'] == '/l2':
-            bot.sendMessage(chat_id, 'https://telegra.ph/SHifr-Vizhenera-11-06')#2lection
+            bot.sendMessage(chat_id, 'https://uk.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%92%D1%96%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%B0')#2lection
         elif msg['text'] == '/l3':
-            bot.sendMessage(chat_id, 'none')#3lection
+            bot.sendMessage(chat_id, 'https://telegra.ph/Mashina-Bebb%D1%96dzhaanal%D1%96tichna-mashina--pershij-prototip-suchasnogo-kompyutera-11-07')#3lection
         elif msg['text'] == '/l4':
             bot.sendMessage(chat_id, 'still nothing')#4lection
         elif msg['text'] == '/l5':
@@ -42,9 +42,9 @@ def handle(msg):
         elif msg['text'] == '/practice':
             bot.sendMessage(chat_id, '/p1 - Ceasar \n /p2 \n /p3  \n /p4 \n /p5')
         elif msg['text'] == '/p1':
-            bot.sendMessage(chat_id,'Cheers!' ) #1task
+            bot.sendMessage(chat_id, ' https://telegra.ph/Zadacha-2-SHifr-Vizheneral2-11-07' ) #1task
         elif msg['text'] == '/p2':
-            bot.sendMessage(chat_id, 'AVADA KEDAWRA')#2task
+            bot.sendMessage(chat_id, '...')#2task
         elif msg['text'] == '/p3':
             bot.sendMessage(chat_id, 'ENIGMA ONE LOVE')#3task
         elif msg['text'] == '/p4':
@@ -56,9 +56,9 @@ def handle(msg):
 
             #
     elif content_type == 'text':
-       bot.sendMessage(chat_id, 'Невідомий текст. Натисніть /help для детальнішої інформації.')
+       bot.sendMessage(chat_id, 'Невідомий текст: /menu \n Натисніть /help для детальнішої інформації.')
     elif content_type != 'text':
-     bot.sendMessage(chat_id, 'Невідомий текст. Натисніть /help для детальнішої інформації. ')
+     bot.sendMessage(chat_id, 'Невідомий текст: /menu \n Натисніть /help для детальнішої інформації.')
 
 print (authentication.load_users())
 
